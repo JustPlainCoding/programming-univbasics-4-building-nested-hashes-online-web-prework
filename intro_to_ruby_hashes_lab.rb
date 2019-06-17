84717293eaf7c348ea72f05e3c0e62d24a5e4786
Eric J Wilhite <ericjwilhite@gmail.com>
2019-06-17 19:06:24 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -8,8 +8,8 @@ def monopoly_with_second_tier
 	monopoly = {
 	  railroads: {
 	    pieces: 4,
-	    rent_in_dollars: {},
-	    names: {}
+	    names: {},
+	    rent_in_dollars: {}
 	  }
 	}
 end
@@ -18,8 +18,8 @@ def monopoly_with_third_tier
 	monopoly = {
 	  railroads: {
 	    pieces: 4,
-	    rent_in_dollars: {},
-	    names: {}
+	    names: {},
+	    rent_in_dollars: {}
 	  }
 	}
 end
